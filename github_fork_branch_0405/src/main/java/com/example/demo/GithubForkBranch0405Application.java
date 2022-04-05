9ce2621d651a7514054b21719228bebf856cf992
me <bluejeansh@hanmail.net>
2022-04-05 16:50:57 +0900

    1 팀장 commit

---
+++ github_fork_branch_0405/src/main/java/com/example/demo/GithubForkBranch0405Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubForkBranch0405Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubForkBranch0405Application.class, args);
+		System.out.println("깃허브 포크해보기");
+	}
+
+}
