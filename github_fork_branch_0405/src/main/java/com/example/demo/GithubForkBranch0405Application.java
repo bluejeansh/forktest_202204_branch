596f571de0ae7d6701b14f45e4b88ba14facc955
me <bluejeansh@hanmail.net>
2022-04-06 10:56:28 +0900

    5 scjper commit

--- github_fork_branch_0405/src/main/java/com/example/demo/GithubForkBranch0405Application.java
+++ github_fork_branch_0405/src/main/java/com/example/demo/GithubForkBranch0405Application.java
@@ -9,6 +9,7 @@ public class GithubForkBranch0405Application {
 	public static void main(String[] args) {
 		SpringApplication.run(GithubForkBranch0405Application.class, args);
 		System.out.println("깃허브 포크해보기");
+		System.out.println("5번째 수정해보기");
 	}
 
 }
